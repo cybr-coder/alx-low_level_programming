29e0ed0f1c73c8358ffd5e348dda73ede0be3554
cybr-coder <codercybr@gmail.com>
2023-07-13 17:59:55 -0700

    Description for main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
+
+/**
+ * main - Prints sting using puts function
+ *
+ * Return - Always 0
+ */
+
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
