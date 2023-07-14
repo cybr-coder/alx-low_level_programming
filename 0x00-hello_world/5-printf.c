33e52e619d72b6b9a29234f1bc25d82dab345aef
cybr-coder <codercybr@gmail.com>
2023-07-13 18:22:28 -0700

    Fix betty errors: trailing whitespaces

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
- * 
- * main - Prints a string using the printf functon
- * 
+ * main - Prints a string using the printf function
  *
  * Return: Always 0
  */
-
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
