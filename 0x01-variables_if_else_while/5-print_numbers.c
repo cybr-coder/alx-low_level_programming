7befa0d5e92ebfc46272332fa8114ff6f2692176
cybr-coder <codercybr@gmail.com>
2023-07-14 18:54:51 -0700

    Print numbers from zero to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print all single numbers of base 10
+ * starting from 0.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%i", num);
+	}
+
+	printf("\n");
+
+	return (0);
+}
