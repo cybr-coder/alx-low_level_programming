fd118ca4594b1190b31fce2f926ca23cee9b35a8
cybr-coder <codercybr@gmail.com>
2023-07-14 18:42:03 -0700

    Print alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase
+ * except 'q' and 'e'.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
+}
