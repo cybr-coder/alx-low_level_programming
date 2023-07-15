a9c4799eedb613c8198db7ac068d09457dae54ab
cybr-coder <codercybr@gmail.com>
2023-07-14 19:17:02 -0700

    Print numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from zero to nine
+ * without the printf functon.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return(0);
+}
