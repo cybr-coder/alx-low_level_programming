db1d34d5ce10a2289b5d3f6034f8267e1f8458ba
cybr-coder <codercybr@gmail.com>
2023-07-31 23:55:49 -0700

    Add two integers and return a sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Adds two integers and return a sum
+ * @a: First Argurment
+ * @b: Second Argument
+ * Return: Sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+	return (sum);
+}
