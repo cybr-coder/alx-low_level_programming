c46a78fa254b9af777bb9819b8e7a68f6f014841
cybr-coder <codercybr@gmail.com>
2023-08-04 04:03:49 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print first 50 fibonacci numbers
+ * starting with 1 and 2
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	int i;
+	long int fib[50];
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	printf("%ld, %ld, ", fib[0], fib[1]);
+	for (i = 2; i < 50; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+		if (i == 49)
+		{
+			printf("%ld\n", fib[i]);
+		}
+		else
+		{
+			printf("%ld, ", fib[i]);
+		}
+	}
+	return (0);
+}
