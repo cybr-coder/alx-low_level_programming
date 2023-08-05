8872a05f7a54813b2b70a87a5c39d80200b6e0a6
cybr-coder <codercybr@gmail.com>
2023-08-05 11:08:49 -0700

    Initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Multiply two integers
+ *
+ * @a: Argument - first number
+ * @b: Argument - second number
+ *
+ * Return: Product of the two integers
+ */
+
+int mul(int a, int b)
+{
+	int product = (a * b);
+	return (product);	
+}
