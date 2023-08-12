29f636943fe5f7a03fc70c94c1a2cfb2d331d4fb
cybr-coder <codercybr@gmail.com>
2023-08-12 04:43:48 -0700

    Initial commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal draws a diaginal line on the terminal
+ *
+ * @n: Number of times the charater '/' should be printed
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
