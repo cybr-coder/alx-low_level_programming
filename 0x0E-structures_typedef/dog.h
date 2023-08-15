e8afce41f5c7ca1473647eb5127e02a43f37c793
cybr-coder <codercybr@gmail.com>
2023-08-14 17:11:22 -0700

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,37 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
