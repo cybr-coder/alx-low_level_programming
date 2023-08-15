65ef19a0784d52e29f2abb8ead82dc8a2094854a
cybr-coder <codercybr@gmail.com>
2023-08-14 17:21:14 -0700

    Initial commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initialize a variable of the type struct dog
+ * @d: points to dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
