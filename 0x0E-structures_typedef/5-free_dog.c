e45de4e3c8b498474431fb7e9d4785d5a60e7364
cybr-coder <codercybr@gmail.com>
2023-08-15 18:05:37 -0700

    Initial commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free instance of struct dog
+ * @d: instance to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
