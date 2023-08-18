fc73f76fa1c4b36b7eee6f927e6b22c4f15a4e7c
cybr-coder <codercybr@gmail.com>
2023-08-17 18:00:16 -0700

    Initial commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Seach for an integer
+ * @size: number of elements in the array
+ * @cmp: pointer to the function used to compare values
+ * Return: index of where integer was found
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+	}
+	return (-1);
+}
