0e69a4003bb9e59f8f127b42182fdc2cfbce8d33
cybr-coder <codercybr@gmail.com>
2023-08-28 06:39:44 -0700

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes a pointer to an int as parameter
+ * and updates the values it points to to 98.
+ *
+ * @n: The pointer
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
