2b2f6dde961f9034e9b560aa2565bd8a3953f1ba
Lawrence Kelvin Nyarko <codercybr@gmail.com>
2023-09-01 17:42:38 -0700

    Initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: count of command line arguments
+ * @argv: Index of arguments
+ * Return: 0
+ */
+
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
