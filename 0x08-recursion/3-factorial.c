137eedb301bd49d336280445b8632dce021d8cd5
Lawrence Kelvin Nyarko <codercybr@gmail.com>
2023-09-06 02:56:32 -0700

    Initial commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * fatorial - Returns the factorial of a given number
+ * @n: Given number
+ * Return: The factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0 || n == 1)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
