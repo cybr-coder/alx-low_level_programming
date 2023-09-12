85bd633ea711dbeb5912419249b8eb6d8790cc58
Lawrence Kelvin Nyarko <codercybr@gmail.com>
2023-09-12 15:16:36 -0700

    Initial commit

---
+++ 0x0E-structure_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents information about dog
+ *
+ * @name: Name of dog as a string.
+ * @age: Age of dog as a float.
+ * @owner: Owner of dog as a string.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
